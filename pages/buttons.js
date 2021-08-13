4a1195160ececdeee3781f8996148d1a44efe64f
Salman Javed <simply.ingenious@gmail.com>
2021-08-13 19:23:21 +0500

    added buttons and cards pages

---
+++ pages/buttons.js
@@ -0,0 +1,7 @@
+import styles from "../styles/buttons.module.scss";
+
+const Buttons = () => {
+  return <div>Code the button component here</div>;
+};
+
+export default Buttons;
---
+++ pages/cards.js
@@ -0,0 +1,7 @@
+import styles from "../styles/cards.module.scss";
+
+const Cards = () => {
+  return <div>You will code the cards here</div>;
+};
+
+export default Cards;
